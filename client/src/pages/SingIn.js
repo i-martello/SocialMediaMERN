4ad28ba9d8aa63f77376f2acb5d1ad5c608e9bac
i-martello <martelloignacio@gmail.com>
2023-03-26 04:44:46 -0300

    cambios

--- client/src/pages/SingIn.js
+++ client/src/pages/SingIn.js
@@ -14,7 +14,7 @@ const SingIn = () => {
 
     e.preventDefault()
 
-    axios.post('https://popawebapi.vercel.app/v13/deployments/login', { name, password }, { withCredentials: true })
+    axios.post('https://popawebapi.vercel.app/login', { name, password }, { withCredentials: true })
       .then(response => {
 
         const { data } = response
