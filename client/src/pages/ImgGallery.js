832331a218397b5776e4f263ae38c83a4d34105b
i-martello <martelloignacio@gmail.com>
2023-03-26 02:31:48 -0300

    cambios

--- client/src/pages/ImgGallery.js
+++ client/src/pages/ImgGallery.js
@@ -14,7 +14,7 @@ const ImgGallery = () => {
 
     (async () => {
       try {
-        const res = await axios.get('http://localhost:4000')
+        const res = await axios.get('https://popaweb.vercel.app/')
         if(!res.data.token){
         console.log(res.data)
         setImage(res.data)
