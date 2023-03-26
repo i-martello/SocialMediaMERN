747c5678a61bf28567c23dbcbfb53d7766857532
i-martello <martelloignacio@gmail.com>
2023-03-26 19:26:19 -0300

    cambios

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -47,7 +47,7 @@ ctrlUser.singin = async (req, res) => {
     }, process.env.JWT_KEY);
     const serialized = serialize("token", token, {
       httpOnly: true,
-      domain: "popaweb.vercel.app",
+      domain: "popawebapi.vercel.app",
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       maxAge: 1000 * 60 * 60 * 24 * 7,
